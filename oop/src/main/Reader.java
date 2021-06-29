88dd02b18f6b194bead88e0eff772a43975d5115
Elena Strakhova <elena.strakhova@mera.com>
2021-06-29 18:21:56 +0300

    new class Reader

---
+++ oop/src/main/Reader.java
@@ -0,0 +1,37 @@
+package main;
+
+public class Reader {
+    String fullName;
+    int readerNumber;
+    String faculty;
+    //Date birthDate;
+    String phoneNumber;
+
+
+    public void takeBook() {
+
+    }
+
+    /*
+    ФИО, номер читательского билета, факультет, дата рождения, телефон. Методы takeBook(), returnBook().
+Разработать программу, в которой создается массив объектов данного класса. Перегрузить методы takeBook(), returnBook():
+     */
+
+    public void returnBook() {
+
+    }
+
+    /*
+     Аналогичным образом перегрузить метод returnBook().
+     Выводит на консоль сообщение "Петров В. В. вернул книги: Приключения, Словарь, Энциклопедия". Или  "Петров В. В. вернул 3 книги".
+     */
+
+    /*
+    ФИО, номер читательского билета, факультет, дата рождения, телефон. Методы takeBook(), returnBook().
+Разработать программу, в которой создается массив объектов данного класса. Перегрузить методы takeBook(), returnBook():
+     */
+
+    public static void main(String[] args) {
+
+    }
+}
