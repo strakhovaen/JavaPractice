7e3cd20571e78eca8a2e8e5b876053ed17564895
strakhovaen <elena.strakhova@mera.com>
2021-06-29 13:11:38 +0300

    new tasks: OOP. + Matrix class

--- oop/src/main/Matrix.java
+++ oop/src/main/Matrix.java
@@ -1,4 +1,17 @@
 package main;
 
 public class Matrix {
+    int array[][];
+    int rows;
+    int columns;
+
+    public static void multiply(int number) {
+
+    }
+
+    public static void main(String[] args) {
+        Matrix matrix1 = new Matrix({{1,2,3},{4,5,6}},2,3);
+
+    }
+
 }
